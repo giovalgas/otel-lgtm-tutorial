ffcd35c1aba5ba65efd901678bfbdb46f6583311
Giovani Valgas <giovalgascom@gmail.com>
2025-06-19 07:09:53 -0300

    feat: enhance API with pageable DTO support

--- api/src/main/java/br/dev/giovalgas/api/Application.java
+++ api/src/main/java/br/dev/giovalgas/api/Application.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "OTEL Tutorial API Docs"))
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class Application {
 
 	public static void main(String[] args) {
